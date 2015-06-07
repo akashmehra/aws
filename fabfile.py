f181de64b5f04cbd661ed6570e241aaf622da476
Akash Mehra <mehra.akash@gmail.com>
2015-06-06 23:08:29 -0700

    Added the fabric task to kill tmux session in which itorch is running.
    Change-Id: I321a648eca0435dadc1596d487b7187df2bc5feb

--- fabfile.py
+++ fabfile.py
@@ -5,13 +5,26 @@ env.shell = '/bin/bash -l -i -c'
 env.user = 'ubuntu'
 
 @parallel
-def nbserver():
-	run('tmux new -s itorch -d "cd $HOME/cvpr && nohup itorch notebook --ip=* --no-browser"')
+def run_nbserver_parallel():
+  run('tmux new -s itorch -d "([ -d $HOME/cvpr ] || mkdir $HOME/cvpr); cd $HOME/cvpr && nohup itorch notebook --ip=* --no-browser"')
+
+@parallel
+def kill_nbserver_parallel():
+  run('tmux kill-session -t itorch')
+
+
+def setup_environment(key_file,hosts_file):
+  env.key_filename = [key_file]
+  env.hosts = [host.replace('\n','').replace('\r','') for host in open(hosts_file).readlines()]
 
 @task
 def start_nbserver(key_file,hosts_file):
-  env.key_filename = [key_file]
-  env.hosts = [host.replace('\n','').replace('\r','') for host in open(hosts_file).readlines()]
-  execute(nbserver)
+  setup_environment(key_file,hosts_file)
+  execute(run_nbserver_parallel)
+
+@task
+def stop_nbserver(key_file,hosts_file):
+  setup_environment(key_file,hosts_file)
+  execute(kill_nbserver_parallel)
   
 
